0338c9f68f160b01979d00b7a753e2e563f3f832
SanDiegoCastilho <sandiegocastilho@gmail.com>
2026-06-10 17:18:55 -0300

    Finalizando questão 2

--- Lista06/q2_using_dd_lib.py
+++ Lista06/q2_using_dd_lib.py
@@ -43,7 +43,53 @@ c8 = bdd_b.add_expr("~r")
 formulab = c1 & c2 & c3 & c4 & c5 & c6 & c7 & c8
 
 exp = "(p → q) → r,s → ¬p,t,¬s ∧ t → q |= r"
-if formula == bdd_a.false:
+if formulab == bdd_b.false:
+    print(f"A expressão {exp} é válida")
+else:
+    print("Não é consequência lógica")
+
+
+#c) (s → p)∨(t → q) |= (s → q)∨(t → p)
+bdd_c = BDD()
+
+bdd_c.declare("s", "p", "t", "q")
+
+#Cláusulas
+c1 = bdd_c.add_expr("(~s) | p | (~t) | q")
+
+#Negação da conclusão
+c2 = bdd_c.add_expr("s")
+c3 = bdd_c.add_expr("(~q)")
+c4 = bdd_c.add_expr("t")
+c5 = bdd_c.add_expr("(~p)")
+
+formulac = c1 & c2 & c3 & c4 & c5
+
+exp = "(s → p)∨(t → q) |= (s → q)∨(t → p)"
+if formulac == bdd_c.false:
+    print(f"A expressão {exp} é válida")
+else:
+    print("Não é consequência lógica")
+
+
+#d) (p ∧ q) → r, r → s,q ∧ ¬s |= ¬p
+bdd_d = BDD()
+
+bdd_d.declare("p", "q", "r", "s")
+
+#Cláusulas
+c1 = bdd_d.add_expr("(~p) | (~q) | r")
+c2 = bdd_d.add_expr("(~r) | s")
+c3 = bdd_d.add_expr("q")
+c4 = bdd_d.add_expr("(~s)")
+
+#Negação da conclusão
+c5 = bdd_d.add_expr("p")
+
+formulad = c1 & c2 & c3 & c4 & c5
+
+exp = "(p ∧ q) → r, r → s,q ∧ ¬s |= ¬p"
+if formulad == bdd_d.false:
     print(f"A expressão {exp} é válida")
 else:
     print("Não é consequência lógica")
